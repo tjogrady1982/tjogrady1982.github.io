8a1d2d63fd2cb2340b3cf1fe6197bd4926a0af5e
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2018-09-11 14:35:48 +0100

    fixed scripts file

--- Scripts.js
+++ Scripts.js
@@ -10,10 +10,11 @@ function createParagraph() {
     var para = document.createElement('p');
     para.textContent = 'You clicked the button!';
     document.body.appendChild(para);
-  }
-   
-  var buttons = document.querySelectorAll('button');
+    var buttons = document.querySelectorAll('button');
    
   for (var i = 0; i < buttons.length ; i++) {
     buttons[i].addEventListener('click', createParagraph);
   }
+  }
+   
+  
